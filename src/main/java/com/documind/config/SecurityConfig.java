38143448433d0d8471807a1540f5aeb5c8b1bd38
Aditya Gupta <adityacse207@gmail.com>
2026-05-30 23:42:28 +0530

    feat: update url for cors

--- src/main/java/com/documind/config/SecurityConfig.java
+++ src/main/java/com/documind/config/SecurityConfig.java
@@ -47,7 +47,8 @@ public class SecurityConfig {
             "http://localhost:3000",
             "https://xadisri.in",
             "https://www.xadisri.in",
-            "https://documind.xadisri.in"
+            "https://documind.xadisri.in",
+            "https://documind-frontend-git-main-aditya19guptas-projects.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"));
